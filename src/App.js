0a98a1273dcea1605b190b2e7173a5f041c32e84
Ben Kopchains <bkopchains@dynamis.com>
2020-03-31 17:49:51 -0400

    adding some stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import react from './react.png';
+import Select from 'react-select'
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={react} className="App-logo" alt="logo" />
         Ben Kopchains - Test App
       </header>
     </div>
+    <div>
+      <Select />
+    </div>
+    </>
   );
 }
 
