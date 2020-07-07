08c1d6bd630609b5b171b154a0025cd04d89a848
Ben Kopchains <bkopchains@dynamis.com>
2020-07-07 11:06:11 -0400

    Updates

--- precache-manifest.f3d559df1af0036cb8c9588636fbd30b.js
+++ precache-manifest.f3d559df1af0036cb8c9588636fbd30b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6399d822aaefb46ca586a4d5b1729e06",
+    "revision": "73b64bea838c7057b4377f28af21a5a2",
     "url": "/website/index.html"
   },
   {
-    "revision": "630441a0d089bd048e70",
-    "url": "/website/static/css/main.26c08944.chunk.css"
+    "revision": "008e65052c514f677562",
+    "url": "/website/static/css/main.89ef2c10.chunk.css"
   },
   {
     "revision": "a0ccc553e01cefe2d421",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/website/static/js/2.5dd3540e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "630441a0d089bd048e70",
-    "url": "/website/static/js/main.34e96e93.chunk.js"
+    "revision": "008e65052c514f677562",
+    "url": "/website/static/js/main.6b8be670.chunk.js"
   },
   {
     "revision": "592b0129f9f391a6d401",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.f5f58391583c2a57d6fc1893b328419b.js"
+  "/website/precache-manifest.f3d559df1af0036cb8c9588636fbd30b.js"
 );
 
 self.addEventListener('message', (event) => {
